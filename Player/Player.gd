bdd93240476e566389b82e0a8231eb807e57d8eb
lijs8 <lijs8@lenovo.com>
2023-09-18 18:52:15 +0800

    feat: 地图瓦片

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,11 +3,19 @@ extends CharacterBody2D
 const MAX_SPEED = 80
 const ACCELERATION = 500  # 加速度
 const FRICTION = 500 # 摩擦力
+
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anmiationState = $AnimationTree.get('parameters/playback')
+enum {
+	MOVE,
+	ATTACK,
+	ROLL
+}
+
+var state = MOVE
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,13 +34,32 @@ func _physics_process(delta):
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 	#move_and_slide()
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			roll_state(delta)
+
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO :
+		$AnimationTree.set('parameters/Idle/blend_position', input_vector)
+		$AnimationTree.set('parameters/Walk/blend_position', input_vector)
+		anmiationState.travel('Walk')
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		anmiationState.travel('Idle')
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide()
-
+	
+func attack_state(delta):
+	pass
+	
+func roll_state(delta):
+	pass
